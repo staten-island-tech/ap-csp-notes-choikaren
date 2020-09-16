129f6c9e8a31ae63b35de159e29d71619b8281e1
choikaren <57500302+choikaren@users.noreply.github.com>
2020-09-16 16:53:42 -0400

    NOTES 09.16.2020

---
+++ index.js
@@ -0,0 +1,28 @@
+var life = 100;
+
+life = life - 10 ;
+life = life - 10 ;
+
+var name = 'Karen Choi';
+// things inside quotes are a string
+
+
+//BOOLEAN
+var checkout = false;
+// dont need quotes when terms are reserved
+
+var box; 
+//var undefined in console log bc no value
+
+var square = null;
+//var has no value
+
+const life = 100;
+life = 50;
+// w const, you cannot redeclare or modify values as you can w var;
+
+let life = 50; 
+life = life - 10;
+//w let, you can modify values as you can w var, but can not be redeclared
+
+console.log ('Hello!');
